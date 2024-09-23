f3ca2e22d189a69383d1791b4713d03ad5463be6
中東優輔 <nsantamaria100gou@gmail.com>
2024-09-23 22:39:43 +0900

    修正

--- js/main.js
+++ js/main.js
@@ -1,7 +1,6 @@
 document.addEventListener("DOMContentLoaded", function() {
     var navbar = document.getElementById("navbar");
     var navbarOffsetTop = navbar.offsetTop;
-
     window.addEventListener("scroll", function() {
         if (window.pageYOffset >= navbarOffsetTop) {
             navbar.classList.add("fixed");
